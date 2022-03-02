42b2a018affe780e87b55f32d121bb58155ef7ff
sahayana <saokhueyang@gmail.com>
2022-03-02 18:13:59 +0900

    navbar fixed

--- config/settings.py
+++ config/settings.py
@@ -153,7 +153,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # # S3
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 
 AWS_S3_REGION_NAME = "ap-northeast-2"
@@ -163,7 +163,7 @@ AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_KEY")
 AWS_STORAGE_BUCKET_NAME = os.environ.get("BUCKET_NAME")
 AWS_DEFAULT_ACL = "public-read"  # 올린 파일을 누구나 읽을 수 있게 지정합니다!
 
-try:
-    from config.local_settings import *
-except ImportError:
-    pass
+# try:
+#     from config.local_settings import *
+# except ImportError:
+#     pass
